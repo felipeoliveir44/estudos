20751c99973a0b5c70dbca40ebff426c5648b493
Felipe <felipeoliveiragg@outlook.com>
2022-12-31 18:02:28 -0300

    Diferenças entre VAR e LET

---
+++ js_basico/aula-9/index.js
@@ -0,0 +1,18 @@
+// Diferença entre var e let é que com var podemos redeclarar a variavel, com let isso nao é possivel
+
+
+var nome = 'luiz';
+
+console.log(nome);
+
+var nome = 'felipe';
+
+console.log(nome);
+
+let nome2 = 'luiz';
+
+console.log(nome2);
+
+//let nome2 = 'felipe';
+
+console.log(nome2);
