375caca582e4c8c8ecfe6e15a6f10d9724efabb2
Felipe <felipeoliveiragg@outlook.com>
2022-12-31 18:32:31 -0300

    IF ELSE

---
+++ logica_programacao/aula-4/index.js
@@ -0,0 +1,13 @@
+// if else if else
+
+const hora = 19;
+
+if (hora >= 0 && hora <= 12) {
+    console.log('Bom dia');
+}
+else if (hora => 12 && hora <= 18) {
+    console.log('Boa tarde');
+}
+else {
+    console.log('Boa noite');
+}
