cd850be8b6d3768885ab50b26b720f56098c60eb
Felipe <felipeoliveiragg@outlook.com>
2022-12-31 18:33:26 -0300

    Operadores ternarios

---
+++ logica_programacao/aula-5/index.js
@@ -0,0 +1,16 @@
+// Operadores ternários
+// ? :
+
+const pontuacaoUsuario = 1000;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usúario VIP' : 'Usuário normal';
+
+console.log(nivelUsuario);
+
+// if(pontuacaoUsuario >= 1000) {
+//     console.log('Usúario VIP');
+// }
+// else {
+//     console.log('Usuário normal');
+// }
+
+
