ac1c59ab1c03890d3bab9429c9f1b25aeb778e24
Felipe <felipeoliveiragg@outlook.com>
2022-12-31 18:04:58 -0300

    Dados primitivos

---
+++ js_basico/aula-10/index.js
@@ -0,0 +1,12 @@
+// Dados primitivos
+
+const nome = 'luiz'; // string
+const nome1 = "luiz"; // string
+const nome2 = `luiz`; //string 
+
+const num1 = 10; //number
+const num2 = 10.10 //number
+
+let nomeAluno; // undefined -> nao aponta pra local nenhuma na memoria
+const sobrenomeAluno = null; //nulo -> nao aponta pra local nenhuma na memoria
+const aprovado = true; 
