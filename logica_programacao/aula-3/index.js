8c17b4ec4fa8e471ab1291aac2f103612a207a08
Felipe <felipeoliveiragg@outlook.com>
2022-12-31 18:31:58 -0300

    Falsy

---
+++ logica_programacao/aula-3/index.js
@@ -0,0 +1,19 @@
+/*
+
+&& -> false && true -> false 
+|| ->
+*/
+
+
+/*
+falsy values / Não é literal
+* False / Literal
+0
+'' "" ``
+null / undefined
+NaN *
+
+*/
+console.log('Luiz' && true && 'Maria');
+
+
