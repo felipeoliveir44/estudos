c84ec6f7fc32e505b050a94c960d96bce77d3ec0
Felipe <felipeoliveiragg@outlook.com>
2022-12-31 18:31:20 -0300

    Operadores logicos

---
+++ logica_programacao/aula-2/index.js
@@ -0,0 +1,24 @@
+/* Operadores logicos
+
+&& -> And -> E / Todas as expressoes precisam ser verdadeiras para retornar true
+|| -> OR -> Ou / Uma das expressoes precisam ser verdadeiras para retornar true
+! -> NOT -> Não 
+*/
+
+
+if (10 && 12 > 5) {
+    console.log('É maior');
+}
+else {
+    console.log('É menor');
+}
+
+if (10 || 3 > 5) {
+    console.log('É maior');
+}
+else {
+    console.log('É menor');
+}
+
+console.log(!false) // True
+
