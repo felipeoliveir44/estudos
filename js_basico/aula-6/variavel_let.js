fdf86e0c358d9e674f66a692dab68ac20eb7bca9
Felipe <felipeoliveiragg@outlook.com>
2022-12-31 17:56:48 -0300

    Variaveis - Let

---
+++ js_basico/aula-6/variavel_let.js
@@ -0,0 +1,10 @@
+let nome = "Luiz"; // Declarou a variavel
+
+console.log(nome, 'Nasceu em 2003'); // Inicializando a variavel
+
+nome = "Felipe";
+
+// Nao pode começar o nome de uma variavel com um numero
+// Não podemos redeclarar com let
+
+console.log(nome, 'Nasceu em 2003')
