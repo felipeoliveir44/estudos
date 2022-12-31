c34a9eb1f5cafa108283f80483a7e24b4250e5f5
Felipe <felipeoliveiragg@outlook.com>
2022-12-31 18:38:32 -0300

    For in

---
+++ logica_programacao/aula-12/index.js
@@ -0,0 +1,19 @@
+// For in
+
+const frutas = ['Pera', 'Maça', 'Uva'];
+
+// for (let i in frutas) { // Está lendo os indices do array
+//     console.log(i);
+// }
+
+const pessoa = {
+    nome: 'Luiz',
+    sobrenome: 'Felipe',
+    idade: 19
+}
+
+for(let chaves in pessoa ) {
+    console.log(chaves);
+}
+
+
