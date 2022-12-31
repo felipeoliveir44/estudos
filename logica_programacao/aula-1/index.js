e986af6b08e24db3b0924243f31c4dfff84ced37
Felipe <felipeoliveiragg@outlook.com>
2022-12-31 18:30:47 -0300

    Operadores de comparacao

---
+++ logica_programacao/aula-1/index.js
@@ -0,0 +1,25 @@
+/* Operadores de comparação
+
+> Maior que
+>= Maior/igual
+> menor que
+<= menor/igual
+== igualdade (valor)
+=== igualdade estrita (valor e tipo)
+!= diferente
+!== diferente estrito
+*/
+
+if (10 > 5) {
+    console.log('É maior');
+}
+else {
+    console.log('É')
+}
+
+if (4 < 5) {
+    console.log('É menor');
+}
+else {
+    console.log('É maior');
+}
